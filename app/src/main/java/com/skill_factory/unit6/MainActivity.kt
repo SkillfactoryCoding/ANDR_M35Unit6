4d3de1c4be834bc0389637443c0444482976bc3d
vurtune <kostya.urenevar@gmail.com>
2020-12-01 10:45:25 +0400

    User2 has been added

--- app/src/main/java/com/skill_factory/unit6/MainActivity.kt
+++ app/src/main/java/com/skill_factory/unit6/MainActivity.kt
@@ -19,11 +19,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val user = User(50_000, arrayListOf(Stock("Яндекс", 5000, R.drawable.yandex_icon), Stock("Mail.ru Group", 2175, R.drawable.mailru_icon)))
-        observableUser = ObservableField(user)
+        val user1 = User(50_000, arrayListOf(Stock("Яндекс", 5000, R.drawable.yandex_icon), Stock("Mail.ru Group", 2175, R.drawable.mailru_icon)))
+        val user2 = User(12_000,arrayListOf(Stock("Mail.ru Group", 2175, R.drawable.mailru_icon),Stock("Яндекс", 5000, R.drawable.yandex_icon)))
+        observableUser = ObservableField(user1)
         binding.user = observableUser
         binding.exRate = getExRate()
-
+        //Change this for test data binding
+        observableUser.set(user2)
         binding.f = {
             flag.set(!flag.get())
             if (flag.get()) {
@@ -33,13 +35,16 @@ class MainActivity : AppCompatActivity() {
             }
         }
         binding.flag = flag
-        (binding.recyclerView.adapter) = StockAdapter(user.stockList)
+        (binding.recyclerView.adapter) = observableUser.get()?.stockList?.let { StockAdapter(it) }
         val r = Random(System.currentTimeMillis())
         countDownTimer = object : CountDownTimer(10_000, 1000L) {
             override fun onTick(millisUntilFinished: Long) {
-                user.cash += -1000 + r.nextInt(2000)
-                user.stockList.forEach {
-                    it.price += (-0.1 * it.price).toInt() + r.nextInt((0.2 * it.price).toInt())
+                val u: User? = observableUser.get()
+                if(u != null) {
+                    u.cash += -1000 + r.nextInt(2000)
+                    u.stockList.forEach {
+                        it.price += (-0.1 * it.price).toInt() + r.nextInt((0.2 * it.price).toInt())
+                    }
                 }
             }
 
